2fe419ec5b663aae481c95df26e68f656ccfdb80
Luke Todd <luke_todd9@hotmail.com>
2025-04-18 11:59:24 -0600

    Create hvac_utilities.py

---
+++ other/hvac_utilities.py
@@ -0,0 +1,24 @@
+Utilities, HVAC, Air Quality
+
+# Electricity
+## Monitoring (usage)
+## Monitoring (protection)
+## Backup (generator)
+## Solar
+
+# Natural Gas
+## Monitoring (usage)
+
+# Water
+## Monitoring (usage)
+## Monitoring (protection)
+## Filtration
+
+# HVAC
+## Zone
+## Thermostat
+
+# Air Quality
+## AQ monitoring
+## Temperature/humidity
+## Weather
